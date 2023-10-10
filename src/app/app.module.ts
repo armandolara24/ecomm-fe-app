578e9d08fdf47681ef2f0f80fc1866875499b9b6
armandolara24 <armandolaramail@gmail.com>
2023-10-10 01:05:06 -0600

    Add Product Details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,42 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Routes, RouterModule, Router } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
+const routes: Routes = [
+  //{ path: 'order-history', component: OrderHistoryComponent, canActivate: [OktaAuthGuard] },
+  //{ path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard] },
+  //{ path: 'login/callback', component: OktaCallbackComponent },
+  //{ path: 'login', component: LoginComponent },
+  //{ path: 'checkout', component: CheckoutComponent },
+  { path: 'search/:keyword', component: ProductListComponent },
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  //{ path: 'cart-details', component: CartDetailsComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
